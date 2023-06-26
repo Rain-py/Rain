2ea1a87f15db86bdb6a6214a8dfb3ccb4adefdc3
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-06-27 02:53:14 +0300

    one loop is done

--- Divider/Algo.py
+++ Divider/Algo.py
@@ -13,26 +13,32 @@ class TrainingWorker:
         self.host = host
         self.port = port
         self.path = "./worker/data/"
+        self.config = None
+        self.ID = None
 
     def receive_data(self, ID):
         data = dill.load(open(f"{self.path}{ID}.pkl", "rb"))
         return data
 
     def send_data(self, msg, ID):
-        print("saving data ..")
+        print(f"Algo is saving {msg} ..")
         dill.dump(msg, open(f"{self.path}{ID}_trained.pkl", "wb"))
+        # check if the dump was successful
+        data = dill.load(open(f"{self.path}{ID}_trained.pkl", "rb"))
+        print(f"Algo saved {data} ..")
+        print("sending data to coordinator")
 
-    def calculate_gradient(self, model, X_train, y_train, config):
-        if config["lib"] == "tensorflow":
+    def calculate_gradient(self, model, X_train, y_train):
+        if self.config ["lib"] == "tensorflow":
             old_weights = model.get_weights()
-            model.compile(loss=config["loss"], optimizer=config["optimizer"], metrics=['accuracy'])
-            model.fit(X_train, y_train, epochs=config["epochs"], batch_size=config["batch_size"])
+            model.compile(loss=self.config ["loss"], optimizer=self.config ["optimizer"], metrics=['accuracy'])
+            model.fit(X_train, y_train, epochs=self.config ["epochs"], batch_size=self.config ["batch_size"])
             new_weights = model.get_weights()
-            gradients = [(new_weights[i] - old_weights[i]) / -config["lr"] for i in range(len(new_weights))]
+            gradients = [(new_weights[i] - old_weights[i]) / -self.config ["lr"] for i in range(len(new_weights))]
             return gradients
 
 
-        elif config["lib"] == "pytorch":
+        elif self.config ["lib"] == "pytorch":
             # Convert numpy arrays to PyTorch tensors
             X_train = torch.from_numpy(X_train).float()
             y_train = torch.from_numpy(y_train).long()
@@ -41,13 +47,13 @@ class TrainingWorker:
             train_dataset = TensorDataset(X_train, y_train)
 
             # TODO: Remove hardcoding
-            config["optimizer"] = optim.Adam(model.parameters(), lr=0.001)
+            self.config ["optimizer"] = optim.Adam(model.parameters(), lr=0.001)
 
             # Create a DataLoader for the training dataset with the defined batch size
-            train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=False)
+            train_loader = DataLoader(train_dataset, batch_size=self.config ["batch_size"], shuffle=False)
             
             old_weights = [param.clone() for param in model.parameters()]
-            for epoch in range(config["epochs"]):
+            for epoch in range(self.config ["epochs"]):
                 total_correct = 0
                 total_samples = 0
                 total_loss = 0
@@ -55,12 +61,12 @@ class TrainingWorker:
                 for i, (data, labels) in enumerate(train_loader):
                     # Forward pass
                     outputs = model(data)
-                    loss = config["loss"](outputs, labels)
+                    loss = self.config ["loss"](outputs, labels)
 
                     # Backward and optimize
-                    config["optimizer"].zero_grad()
+                    self.config ["optimizer"].zero_grad()
                     loss.backward()
-                    config["optimizer"].step()
+                    self.config ["optimizer"].step()
 
                     # Compute training accuracy
                     _, predicted = torch.max(outputs.data, 1)
@@ -72,35 +78,31 @@ class TrainingWorker:
                 epoch_loss = total_loss / (i + 1)
                 epoch_accuracy = total_correct / total_samples
                 print('Epoch [{}/{}], Loss: {:.4f}, Accuracy: {:.4f}'
-                    .format(epoch+1, config["epochs"], epoch_loss, epoch_accuracy))
+                    .format(epoch+1, self.config ["epochs"], epoch_loss, epoch_accuracy))
 
             new_weights = [param.clone() for param in model.parameters()]
-            gradient = [(new_weights[i] - old_weights[i]) / -config["lr"] for i in range(len(new_weights))]
+            gradient = [(new_weights[i] - old_weights[i]) / -self.config ["lr"] for i in range(len(new_weights))]
             gradient = [x.detach().numpy() for x in gradient]
             return gradient
     
 
     def run(self):
-        X_train = None
-        y_train = None
-        config = None
-        ID = sys.argv[1]
-        print("Worker", ID, "started")
+        self.ID = sys.argv[1]
+        print("Worker", self.ID, "started")
         # while True:
-        data = self.receive_data(ID)
-        print(data)
-        if data[0] == "initial":
-            data = data[1]
-            ID = data["ID"]
-            config = data["config"]
-            X_train = np.load(f"{self.path}/X_train_{ID}.npy")
-            y_train = np.load(f"{self.path}/y_train_{ID}.npy")
-            self.send_data("ready", ID)
-        elif data[0] == "train":
-            model = data[1]
-            gradient = self.calculate_gradient(model, X_train, y_train, config)
-            # Send gradient to the server
-            self.send_data(gradient, ID)
+        data = self.receive_data(self.ID)[0]
+        print(f"Algo received {data} ..")
+
+        model = data["model"]
+        self.config = data["config"]
+        print(f"The algo config is {self.config}")
+
+        X_train = np.load(f"{self.path}/X_train_{self.ID}.npy")
+        y_train = np.load(f"{self.path}/y_train_{self.ID}.npy")
+        gradient = self.calculate_gradient(model, X_train, y_train)
+        
+        # Send gradient to the server
+        self.send_data(gradient, self.ID)
 
 
 
