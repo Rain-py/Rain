63460bad3ee073a0eb3f9d626d0fa8ffff6fd648
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-07-02 22:50:12 +0300

    add logs dir to save logs

--- LogService/LogService.py
+++ LogService/LogService.py
@@ -1,6 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
-
+import os
 
 class LogService:
 
@@ -11,7 +11,10 @@ class LogService:
         formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(name)s] %(message)s')
         console_handler = logging.StreamHandler()  # Output logs to the console
         console_handler.setFormatter(formatter)
-        file_handler = RotatingFileHandler(f"{name}.log", maxBytes=1024*20, backupCount=0)  # Output logs to a rotating file
+        log_file_path = f"./logs/{name}.log"
+        if not os.path.exists(log_file_path):
+            os.makedirs(log_file_path) 
+        file_handler = RotatingFileHandler(log_file_path, maxBytes=1024*20, backupCount=0)  # Output logs to a rotating file
         file_handler.setFormatter(formatter)
         self.logger.addHandler(console_handler)
         self.logger.addHandler(file_handler)
