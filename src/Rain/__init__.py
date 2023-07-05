a5f6887b26aed00ca44f9939deabddf716c48993
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-07-05 18:35:59 +0300

    worker can accept a port number

--- src/Rain/__init__.py
+++ src/Rain/__init__.py
@@ -1,11 +1,19 @@
 from Rain.Worker.WorkerAmbassador import WorkerAmbassador
+import argparse
+
 BASE_PORT = 50151
 
-def start_rain_worker():
+def start_rain_worker(port=BASE_PORT):
     print(f"A worker will be instantiated...")
-    worker = WorkerAmbassador(BASE_PORT)
-    print(f"The worker will serve on port:{BASE_PORT}")
+    worker = WorkerAmbassador(port)
+    print(f"The worker will serve on port: {port}")
     worker.serve()
-    print(f"The worker is serving on port:{BASE_PORT} now!")
+    print(f"The worker is serving on port: {port} now!")
     worker.wait_for_termination()
     print(f"The worker is terminated!")
+
+def main():
+    parser = argparse.ArgumentParser(description="Rain Worker Starter")
+    parser.add_argument("--port", type=int, default=BASE_PORT, help="Port number for the worker to serve on")
+    args = parser.parse_args()
+    start_rain_worker(args.port)
