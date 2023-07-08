ab55d662d0319162ec4d8873e97897cfa4085028
mostafa-elgendy22 <moselgendy@hotmail.com>
2023-07-08 21:50:41 +0300

    Semi sync mode done

--- src/Rain/Rain.py
+++ src/Rain/Rain.py
@@ -1,11 +1,10 @@
-
-
 from Rain.Provisioner.Provisioner import Provisioner
 from Rain.Divider.DividerProxy import DividerProxy
 from Rain.LogService.LogService import LogService
 from Rain.TemporaryFilesManager.TemporaryFilesManager import TemporaryFilesManager
-# from Rain.ConfigHandler.ConfigHandler import ConfigHandler
-import numpy
+import numpy as np
+
+
 class Rain:
     def __init__(self, config : dict, model : any = None) -> None:
       """
@@ -44,13 +43,13 @@ class Rain:
       except Exception as e:
           self.logger.log('error', f"Error deleting: {e}")
 
-    def train(self, X_train : numpy.ndarray, y_train : numpy.ndarray, strategy : str = 'sync') -> any:
+    def train(self, X_train : np.ndarray, y_train : np.ndarray, strategy : str = 'sync') -> any:
       """
       Function to train the model.
       
       Args:
-          X_train (numpy.ndarray): X train data
-          y_train (numpy.ndarray): y train data (if unsupervised, pass None)
+          X_train (np.ndarray): X train data
+          y_train (np.ndarray): y train data (if unsupervised, pass None)
           strategy (str, optional): strategy to update the model gradients. Defaults to 'sync'.
 
       Returns:
